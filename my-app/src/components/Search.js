064f313dc6d4a22a3d1f42c487a32a2134003b29
Firdosh01 <firdosh7392@gmail.com>
2023-03-28 23:25:13 -0500

    new project: Zomato-Restaurants-Cards

---
+++ my-app/src/components/Search.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import style from './style.css'
+import Fooddata from './FoodData'
+import Cards from './Cards'
+
+function Search() {
+
+    const [fdata, setfdata] = useState(Fooddata)
+    console.log(fdata)
+
+    const zomatologo = "https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png"
+  return (
+    <>
+    <div className='wrapper'>
+        <img src={zomatologo} alt="" />
+        <h2 style={{ color: "#1b1464", cursor: "pointer" }} className="mt-3">Search Filter App</h2>
+    </div>
+
+    <form className='form'>
+        <input type="text" placeholder='Search for restaurant, cuision or a dish' />
+        <button className='btn'>Submit</button>
+
+    </form>
+
+    <section className="item-section mt-4 container">
+        <h2 className="font-normal ">Inspiration for your first order</h2>
+
+        <div className="row">
+            <Cards />
+        </div>
+
+    </section>
+
+    </>
+  )
+}
+
+export default Search
